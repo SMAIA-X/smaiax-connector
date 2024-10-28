676b6d084f4a0c13f0a0f76dc5726dfa2e7bcb97
Adrian <adrian.essig@outlook.com>
2024-10-28 19:06:49 +0100

    Registered MqttReader, MqttSettings and BackgroundService

--- SMAIAXConnector/Program.cs
+++ SMAIAXConnector/Program.cs
@@ -1,14 +1,22 @@
-﻿namespace SMAIAXConnector;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SMAIAXConnector.Messaging;
+
+namespace SMAIAXConnector;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
-    
-    static void ConnectToMqtt()
-    {
-        // Connect to MQTT
+
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<MessagingBackgroundService>();
+                services.Configure<MqttSettings>(context.Configuration.GetSection("MQTT"));
+                services.AddSingleton<IMqttReader, MqttReader>();
+            })
+            .Build();
+        await host.RunAsync(); 
     }
 }
