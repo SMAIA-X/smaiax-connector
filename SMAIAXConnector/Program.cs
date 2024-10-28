3230415491098e87560626966947d20a0a090010
Adrian <adrian.essig@outlook.com>
2024-10-28 14:50:35 +0100

    initial project setup

---
+++ SMAIAXConnector/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SMAIAXConnector;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    
+    static void ConnectToMqtt()
+    {
+        // Connect to MQTT
+    }
+}
